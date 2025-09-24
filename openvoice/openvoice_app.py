d923ed1a352feaf3f67efd499b2b621119206bb6
Eddy Gangloff <eddy.gangloff@gmail.com>
2025-09-24 09:23:44 +0700

    Limit fron 200 characters to 2000 words

--- openvoice/openvoice_app.py
+++ openvoice/openvoice_app.py
@@ -102,10 +102,10 @@ def predict(prompt, style, audio_file_pth, agree):
             None,
             None,
         )
-    if len(prompt) > 200:
-        text_hint += f"[ERROR] Text length limited to 200 characters for this demo, please try shorter text. You can clone our open-source repo and try for your usage \n"
+    if len(prompt.split()) > 2000:
+        text_hint += f"[ERROR] Text length limited to 2000 words. \n"
         gr.Warning(
-            "Text length limited to 200 characters for this demo, please try shorter text. You can clone our open-source repo for your usage"
+            "Text length limited to 2000 words, please try shorter text."
         )
         return (
             text_hint,
